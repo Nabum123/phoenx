a4da3759c5c01350a11d934cd3c19bbc6352969c
Nabum123 <87659064+Nabum123@users.noreply.github.com>
2021-08-05 14:03:45 +0300

    Update Start2.bat

--- Start2.bat
+++ Start2.bat
@@ -1,2 +1,2 @@
-Moac.exe -coin ETH -pool s-jp.comining.io:9999 -wal A2pySTrK3UND6A24JUbj6eU.worker -pass x -log 0 -proto 4
+Moac.exe -coin ETH -rmode 1 -pool stratum+tcp://daggerhashimoto.eu-west.nicehash.com:3353 -ewal 3CdLsyKu3kmbumz3H1U937Y8H2kSBto62s -esm 3 -allpools 1 -allcoins 0
 pause
